1473505b52b062a889bb53e60ddc7690c89525e4
rishijain <jainrishi.37@gmail.com>
2019-05-28 22:04:52 +0530

    updated authuser service class to handle all use cases

--- app/services/v1/user_auth.rb
+++ app/services/v1/user_auth.rb
@@ -1,11 +1,14 @@
 class V1::UserAuth
-  def initialize(email, password)
+  def initialize(email=nil, password=nil)
     @user = User.find_by email: email
     @password = password
+    @email = email
   end
 
   def authenticate_user
-    if @user.nil?
+    if @email.nil? or @password.nil?
+      { message: 'Pass email and password', status: false }
+    elsif @user.nil?
       { message: 'Incorrect email', status: false }
     else
       @user.authenticate(@password) ? message_for_correct_auth : message_for_incorrect_auth
